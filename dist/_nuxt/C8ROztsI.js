94568dd7f60ba0b0fda6eacf32745077faa864df
ykuanysh98 <ykuanysh98@gmail.com>
2024-12-24 18:28:04 +0500

    delete dist

--- dist/_nuxt/C8ROztsI.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./B3q_52c1.js";const r={};function t(n,_){return o(),c("div",null," subSubCategoryId ")}const a=e(r,[["render",t]]);export{a as default};
--- dist/_nuxt/n8dbhu9g.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./B3q_52c1.js";const o={};function r(t,s){return n(),c("div",null," subCategory ")}const _=e(o,[["render",r]]);export{_ as default};
